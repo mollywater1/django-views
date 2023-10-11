a1be9bc5c2bc5eecaa57f2bbbad1b02459378e67
mollywater1 <ownyx@icloud.com>
2023-10-11 18:55:24 +0400

    new

---
+++ project/simpleapp/templatetags/custom_filter.py
@@ -0,0 +1,15 @@
+from django import template
+
+
+register = template.Library()
+
+CURRENCIES_SYMBOLS = {
+   'rub': 'Р',
+   'usd': '$',
+}
+
+
+@register.filter()
+def currency(value,code='rub'):
+   postfix= CURRENCIES_SYMBOLS[code]
+   return f'{value} {postfix}'
