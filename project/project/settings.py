082619575b078b138ad545700e8d3ad094259486
mollywater1 <ownyx@icloud.com>
2023-10-12 18:53:45 +0400

    new

--- project/project/settings.py
+++ project/project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'simpleapp',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'django_filters'
 ]
 SITE_ID=1
 MIDDLEWARE = [
--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+
+from .filters import ProductFilter
+from .forms import ProductForm
 from .models import Product
-from datetime import datetime
 
 
 class ProductsList(ListView):
@@ -8,9 +13,16 @@ class ProductsList(ListView):
     ordering = 'name'
     template_name = 'products.html'
     context_object_name = 'products'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['next_sale'] = None
+        context['filterset'] = self.filterset
         return context
 
 
@@ -19,3 +31,20 @@ class ProductDetail(DetailView):
     template_name = 'product.html'
     context_object_name = 'product'
 
+
+class ProductCreate(CreateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+
+class ProductUpdate(UpdateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
